88c0c4fb3cca01876df60d4989a58f9f0b76b52a
Stephen Scott <stephenjmscott@gmail.com>
2017-11-14 14:56:07 +0000

    added register

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
-from .forms import UserLoginForm
+from .forms import UserLoginForm , UserRegistrationForm
 
 # Create your views here.
 def get_index(request):
@@ -30,3 +30,23 @@ def login(request):
         
         
     return render(request, "login.html", {'form': form})
+    
+def register(request):
+    if request.method=="POST":
+        form = UserRegistrationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                                     password=form.cleaned_data['password1'])
+                                     
+            if user is not None:
+                auth.login(request, user)
+                return redirect(get_index)
+    else:
+        form = UserRegistrationForm()
+
+    
+    return render(request, "register.html", {'form':form})
